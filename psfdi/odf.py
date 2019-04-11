556f3fa059b545553c755172c0e5bde7fe96b6f3
Sam Potter <spotter1642@gmail.com>
2019-04-11 11:01:29 -0500

    adding functions for computing beta based gamma odf

--- psfdi/odf.py
+++ psfdi/odf.py
@@ -7,6 +7,7 @@
 """
 
 from . import np
+from . import beta
 
 
 def Ifiber(a0, a2, a4, phi, theta):
@@ -122,3 +123,78 @@ def distribution_minfun(params, *args):
     """
 
     return distribution_minimand(params[0], params[1], params[2], params[3], *args)
+
+
+def compute_beta(mean, sd, theta):
+
+    """
+    This function computes the value of a beta distribution with mean and standard deviation (mean, sd) at location
+    theta on the interval [-pi/2, pi/2]
+
+    :param mean: Mean of beta distribution
+    :type mean: float
+    :param sd: Standard deviation of beta distribution
+    :type sd: float
+    :param theta: Argument of Beta distribution pdf. Can be array.
+    :type theta: ndarray
+    :return: Value of specified beta distribution pdf at point theta
+    :rtype: float array_like
+    """
+
+    # Map mean and standard deviation to [0, 1] range of the standard beta distribution
+    mu = (mean + np.pi / 2) / np.pi
+    sigma = sd / np.pi
+
+    # Compute the shape parameters
+    gamma = (mu ** 2 - mu ** 3 - sigma ** 2 * mu) / (sigma ** 2)
+    delta = gamma * (1 - mu) / mu
+
+    # Map evaluation point to [0, 1] range of the standard beta distribution
+    y = (theta + np.pi / 2) / np.pi
+
+    # Evaluate pdf
+    vals = beta.pdf(y, gamma, delta) / np.pi
+
+    return vals
+
+
+def compute_gamma(mean1, sd1, mean2, sd2, theta, d=0.5):
+
+    """
+    This function computes a normalized orientation distribution by combining the results from two separate beta
+    distributions with different means, standard deviations and user defined weighting factor between the distributions
+
+    :param mean1: Mean of first beta distribution
+    :type mean1: float
+    :param sd1: Standard deviation of first beta distribution
+    :type sd1: float
+    :param mean2: Mean of second beta distribution
+    :type mean2: float
+    :param sd2: Standard deviation of second beta distribution
+    :type sd2: float
+    :param theta: Argument of Beta distribution pdf. Can be array.
+    :type theta: ndarray
+    :param d: Optional. Weight factor for adding up the two distributions. Default 0.5
+    :type d: float
+    :return: Normalized value of combined beta distributions
+    :rtype: float array_like
+    """
+
+    # Adjust theta so it wraps around to the -pi/2 pi/2 interval
+    local_theta = np.copy(theta)
+    local_theta[local_theta < -np.pi / 2] = local_theta[local_theta < -np.pi / 2] + np.pi
+    local_theta[local_theta > np.pi / 2] = local_theta[local_theta > np.pi / 2] - np.pi
+
+    # Compute value of distribution pdfs
+    gamma1 = compute_beta(mean1, sd1, local_theta)
+    gamma2 = compute_beta(mean2, sd2, local_theta)
+
+    # Combine distributions using weight factor and shift accordingly
+    gamma = d * (gamma1 + gamma2) + (1 - d) / np.pi
+
+    # Normalize the result so it's an ODF
+    gamma_area = 0.0
+    for i in range(1, len(theta)):
+        gamma_area += 0.5 * (gamma[i] + gamma[i - 1]) * (theta[i] - theta[i - 1])
+
+    return gamma / gamma_area
